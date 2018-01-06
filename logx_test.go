0ad12772dd488dfb9ce3301c673db39e133c441f
Alexander Dorofeev <aka.spin@gmail.com>
2018-01-06 01:57:58 +0100

    add buffer benchmark

--- logx_test.go
+++ logx_test.go
@@ -1,18 +1,20 @@
 package logx_test
 
 import (
+	"bytes"
 	"strconv"
+	"sync"
 	"testing"
 )
 
-func BenchmarkAllocs(b *testing.B) {
+func BenchmarkParamAllocations(b *testing.B) {
 	b.Run(`strings`, func(b *testing.B) {
 		var a string
 		fn := func(param string) {
 			a = param
 		}
-		for i:=1; i<b.N; i++ {
-			func(){
+		for i := 1; i < b.N; i++ {
+			func() {
 				p := []byte(strconv.Itoa(i))
 				fn(string(p))
 			}()
@@ -23,10 +25,44 @@ func BenchmarkAllocs(b *testing.B) {
 		fn := func(param []byte) {
 			a = string(param)
 		}
-		for i:=1; i<b.N; i++ {
-			func(){
+		for i := 1; i < b.N; i++ {
+			func() {
 				p := []byte(strconv.Itoa(i))
 				fn(p)
+			}()
+		}
+	})
+}
+
+func BenchmarkBufferPool(b *testing.B) {
+	b.Run(`byte slice`, func(b *testing.B) {
+		var res bytes.Buffer
+		for i := 0; i < b.N; i++ {
+			func() {
+				var chunk []byte
+				for j := 0; j < 10; j++ {
+					chunk = append(chunk, []byte(strconv.Itoa(j))...)
+				}
+				res.Write(chunk)
+			}()
+		}
+	})
+	b.Run(`buffer pool`, func(b *testing.B) {
+		var res bytes.Buffer
+		pool := &sync.Pool{
+			New: func() interface{} {
+				return &bytes.Buffer{}
+			},
+		}
+		for i := 0; i < b.N; i++ {
+			func() {
+				chunk := pool.Get().(*bytes.Buffer)
+				for j := 0; j < 10; j++ {
+					chunk.Write([]byte(strconv.Itoa(j)))
+				}
+				chunk.WriteTo(&res)
+				chunk.Reset()
+				pool.Put(chunk)
 			}()
 		}
 	})
